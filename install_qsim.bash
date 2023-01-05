752863c623f2301495a9e30a39bfe2b5c34b626b
derwind <uncertainty_principle_ss@yahoo.co.jp>
2023-01-05 20:15:34 +0900

    use cuQuantum 22.07.1

--- install_qsim.bash
+++ install_qsim.bash
@@ -16,15 +16,13 @@ setup_cuda112()
     sudo apt-get -y install cuda
 }
 
-setup_cuda()
+setup_cuQuantum()
 {
-    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-    sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-    wget https://developer.download.nvidia.com/compute/cuda/11.2.0/local_installers/cuda-repo-ubuntu1804-11-2-local_11.2.0-460.27.04-1_amd64.deb
-    sudo dpkg -i cuda-repo-ubuntu1804-11-2-local_11.2.0-460.27.04-1_amd64.deb
-    sudo apt-key add /var/cuda-repo-ubuntu1804-11-2-local/7fa2af80.pub
+    wget https://developer.download.nvidia.com/compute/cuquantum/22.07.1/local_installers/cuquantum-local-repo-ubuntu1804-22.07.1_1.0-1_amd64.deb
+    sudo dpkg -i cuquantum-local-repo-ubuntu1804-22.07.1_1.0-1_amd64.deb
+    sudo cp /var/cuquantum-local-repo-ubuntu1804-22.07.1/cuquantum-*-keyring.gpg /usr/share/keyrings/
     sudo apt-get update
-    sudo apt-get -y install cuda
+    sudo apt-get -y install cuquantum cuquantum-dev cuquantum-doc
 }
 
 setup_qsim()
